471d67ce1b5928e4b4a13406993892b3dbf38591
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-07-09 20:09:09 +0200

    feat(i18n): add Spanish translation for main page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/image', '@nuxtjs/i18n'],
   devtools: { enabled: true },
   ssr: false,
   spaLoadingTemplate: false,
+  css: ['assets/styles/scss/reset.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "sass:map"; @import "assets/styles/scss/mixin.scss";',
+        },
+      },
+    },
+  },
+  i18n: {
+    langDir: 'internationalization',
+    defaultLocale: 'es',
+    locales: [
+      {
+        code: 'es',
+        iso: 'es-ES',
+        file: 'es.json',
+      },
+    ],
+  },
 })
