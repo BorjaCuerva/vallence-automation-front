8ffd9638ace84f5b4a5476ff15cec42da13ef196
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-07-10 20:58:08 +0200

    feat: Add cards grid components

---
+++ components/cardsGrid/CardsBlock.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HeadederOfGrid from './HeaderOfGrid.vue'
+import CardsGrid from './CardsGrid.vue'
+</script>
+<template>
+  <section class="grid-block">
+    <HeadederOfGrid />
+    <CardsGrid />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.grid-block {
+  padding: 0 9.4688rem;
+  display: grid;
+  gap: 3.75rem;
+}
+</style>
---
+++ components/cardsGrid/GridCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import GreenCheck from '@/components/svg/GreenCheck.vue'
+</script>
+<template>
+  <article class="card">
+    <div class="card__icon">
+      <GreenCheck></GreenCheck>
+    </div>
+    <h3 class="card__title">Features</h3>
+    <p class="card__description">
+      Where websites strut their stuff and show off superpowers. No holding back on the
+      bragging rights here!
+    </p>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.card {
+  @include flex(column, flex-start);
+  padding: 1.5rem;
+  border-radius: 0.625rem;
+  background-color: map.get($catalina-blue, 800);
+
+  &__icon {
+    margin-bottom: 1.5rem;
+    width: 5rem;
+  }
+  &__title {
+    font-weight: bold;
+    font-size: 1.5625rem;
+    letter-spacing: -0.0313rem;
+    line-height: 2.1875rem;
+  }
+  &__description {
+    font-size: 1.25rem;
+    letter-spacing: -0.0313rem;
+    line-height: 1.875rem;
+  }
+}
+</style>
