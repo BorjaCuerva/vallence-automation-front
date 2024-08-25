98179bbd9f57143f9ec02ffd44cd021e8d422f01
eduardolopsl <edulop777@gmail.com>
2024-08-25 22:17:06 +0200

    chore(pages): modified index to include text grid block

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import SeparatorBar from '@/components/separatorBar/SeparatorBar.vue'
 import CardsBlock from '@/components/cardsGrid/CardsBlock.vue'
+import TextGridBlock from '~/components/textGrid/TextGridBlock.vue'
 import TheHero from '@/components/TheHero.vue'
 import InfoBlock from '@/components/InfoBlock.vue'
 </script>
 <template>
   <div :style="{ margin: '2rem' }"></div>
   <TheHero />
+  <TextGridBlock />
   <SeparatorBar />
   <CardsBlock />
   <InfoBlock />
